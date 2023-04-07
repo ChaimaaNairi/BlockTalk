8db939c19b30ccf634ad7b3fe3fc9a80406e3b23
ChaimaaN <chaimaanairi91@gmail.com>
2023-04-07 13:30:49 +0300

    commit

--- contracts/BlockTalk.sol
+++ contracts/BlockTalk.sol
@@ -2,9 +2,19 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract BlockTalk {
-    string public message;
     
-    function setMessage(string memory _message) public {
-        message = _message;
+    //user struct
+    struct User {
+        string name;
+        string email;
+        string password;
+        string bio;
+        string profilePic;
+        string[] posts;
+        string[] followers;
+        string[] following;
     }
+
+    
+    
 }
