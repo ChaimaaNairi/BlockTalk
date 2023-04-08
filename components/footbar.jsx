a15fa5d3d9284087d8346df253f6fc6ec684cb94
ChaimaaN <chaimaanairi91@gmail.com>
2023-04-09 00:23:34 +0300

    commit

--- components/footbar.jsx
+++ components/footbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const footbar = () => {
   return (
-    <div>
+    <div className=''>
       footbar
     </div>
   )
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const navbar = () => {
   return (
-    <div className=''>
+    <div className='w-[100%]'>
       navbar
     </div>
   )
