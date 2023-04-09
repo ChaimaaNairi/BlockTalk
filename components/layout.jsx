0ee7d0ff58a2318ed48837860f2bb7e24ced9bd2
ChaimaaN <chaimaanairi91@gmail.com>
2023-04-10 02:28:02 +0300

    commit

---
+++ components/layout.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./navbar";
+import Footer from "./footer"
+
+
+const App = createContext();
+
+const Layout = ({ children }) => {
+
+  return (
+    <App.Provider>
+       
+          <Navbar />
+          {children}
+          <Footer />
+        
+    </App.Provider>
+  );
+}
+
+
+export default Layout;
+export { App };
